11228d453798dd866ee465bfbeefe580c394df53
antodivi93 <156512235+antodivi93@users.noreply.github.com>
2024-11-14 10:20:29 +0100

    Formatted main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-﻿
 // Copyright 2024, Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,30 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-
-
 //////////////////////////////////// START CUSTOMIZABLE VARIABLES ////////////////////////////////////////////////////////////
 
-
 const CAMPAIGN_NAME_CUSTOM_PARAMETER = 'campaignname';
-
 
 // If set to true, it will add the UTM to all your campaigns at campaign level.
 // VERIFY your tracking level before setting it to true.
 const ADD_TO_CAMPAIGN_FINAL_URL_SUFFIX = false;
 
-
-
-
 //////////////////////////////////// END CUSTOMIZABLE PARAMETERS ////////////////////////////////////////////////////////////
-
 
 const FINAL_URL_SUFFIX_TRACKER = `utm_source=google&utm_source_platform=GoogleAds&utm_medium=cpc&utm_campaign={_${CAMPAIGN_NAME_CUSTOM_PARAMETER}}&utm_campaignid={campaignid}`;
 const CUSTOM_PARAMETER_NAME_CHECK = new RegExp("^[a-zA-Z0-9]{1,16}$");
-
-
-
 
 /**
 * Entry point of the script
@@ -48,7 +35,6 @@ function main(){
     throw new Error(`The selected custom parameter name (${CAMPAIGN_NAME_CUSTOM_PARAMETER}) is not valid!`);
   }
 
-
   // Process account according to the type of account this script is running on. 
   if( isThisAccountMCC_() ) {
     processMCCaccounts_();
@@ -57,7 +43,6 @@ function main(){
     processSingleAccount_();
   }
 }
-
 
 /**
  * Checks if current account where the script is running is MCC or not
@@ -68,9 +53,6 @@ function isThisAccountMCC_() {
   return !!this.AdsManagerApp;
 }
 
-
-
-
 /**
  * Iterates every sub account of the MCC account and starts processing them one by one.
 * 
@@ -78,10 +60,8 @@ function isThisAccountMCC_() {
 */
 function processMCCaccounts_(){
 
-
   // Get Accounts Iterator for all sub accounts
   const accountIterator = AdsManagerApp.accounts().get();
-
 
   // Iterate over subaccounts
   while ( accountIterator.hasNext() ) {
@@ -90,9 +70,6 @@ function processMCCaccounts_(){
     processSingleAccount_();
   }
 }
-
-
-
 
 /**
 * Sets current account and starts the processing of its campaigns
@@ -103,14 +80,12 @@ function processSingleAccount_() {
   const currentAccount = AdsApp.currentAccount().getName();
   Logger.log(`Processing Account "${currentAccount}"`);
 
-
   // Retrieve list of campaigns for the account
   const campaignIterator = AdsApp.campaigns().get();
   // Process account's campaigns if there are records
   if(!!campaignIterator && campaignIterator.hasNext()){
     addCustomParameterToCampaigns_(campaignIterator, CAMPAIGN_NAME_CUSTOM_PARAMETER);
   }
-
 
   // Retrieve list of Shopping campaigns for the account
   const shoppingCampaignIterator = AdsApp.shoppingCampaigns().get();
@@ -126,7 +101,6 @@ function processSingleAccount_() {
     addCustomParameterToCampaigns_(pMaxCampaignIterator, CAMPAIGN_NAME_CUSTOM_PARAMETER);
   }
 }
-
 
 /**
 * checks, for all of the Campaigns in the provided iterator, 
@@ -149,14 +123,12 @@ function addCustomParameterToCampaigns_(iterator, customParameterName){
     try {
       const currentCampaignName = replaceInvalidSymbolsInCampaignName_(currentCampaign.getName());
 
-
       // Update/Create Custom Paramter either if missing or campaign name has changed 
       if( Object.keys(currentCampaignParameters).indexOf( customParameterName ) === -1  ||
     currentCampaignParameters[ customParameterName ] !== currentCampaignName) {
         currentCampaignParameters[ customParameterName ] = currentCampaignName;
         currentCampaign.urls().setCustomParameters(currentCampaignParameters);
       }
-
 
       // IF YOU SELECTED it will append the parameters to the campaign url suffix
       if( ADD_TO_CAMPAIGN_FINAL_URL_SUFFIX){
@@ -165,7 +137,6 @@ function addCustomParameterToCampaigns_(iterator, customParameterName){
         if( !!currentCampaignFinalUrlSuffix && currentCampaignFinalUrlSuffix.indexOf(FINAL_URL_SUFFIX_TRACKER) !== -1 ){
           return;
         }
-
 
         // If the tracking parameter is in place
         if( !currentCampaignFinalUrlSuffix ){
@@ -182,9 +153,6 @@ function addCustomParameterToCampaigns_(iterator, customParameterName){
   }
 }
 
-
-
-
 /**
 * Checks if the selected custom paramter name is valid
 * 
@@ -195,11 +163,6 @@ function isCustomParameterNameValid_() {
    const match = CAMPAIGN_NAME_CUSTOM_PARAMETER.match(CUSTOM_PARAMETER_NAME_CHECK);
    return match && CAMPAIGN_NAME_CUSTOM_PARAMETER === match[0];
 }
-
-
-
-
-
 
 /**
 * Replaces invalid characters in campaign name 
@@ -220,4 +183,3 @@ function replaceInvalidSymbolsInCampaignName_(campaignName){
   }
   return newCampaignName.replace(/[^a-zA-Z0-9|;_\/^(!]/g, "_");
 }
-
